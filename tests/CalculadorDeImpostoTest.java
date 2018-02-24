d0087c2c75d8709b51a38dbcd24c4c1d59e7e823
José Francisco da Silva IV <silvaivctd@gmail.com>
2018-02-24 12:13:17 -0300

    TDD Apply

---
+++ tests/CalculadorDeImpostoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadorDeImpostoTest {
+
+    @Test
+    void test() {
+	fail("Not yet implemented"); // TODO
+    }
+
+}
