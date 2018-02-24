fc8a0467ebf473c03e5a37c8c09b1ec4269ab175
José Francisco da Silva IV <silvaivctd@gmail.com>
2018-02-24 12:05:22 -0300

    Add tests

---
+++ tests/OrcamentoTeste.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrcamentoTeste {
+
+    @Test
+    void testGetValor() {
+	double valor = 3.14;
+	Orcamento orc = new Orcamento(valor);
+	assertEquals(valor, orc.getValor());
+    }
+
+}
