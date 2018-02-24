daf7f4f00640c4f705b83e32d9e53f6a1b89676c
José Francisco da Silva IV <silvaivctd@gmail.com>
2018-02-24 11:57:56 -0300

    Implementation of class Budget

--- src/Orcamento.java
+++ src/Orcamento.java
@@ -1,4 +1,17 @@
 
 public class Orcamento {
+    private final double valor;
+
+    public Orcamento(double valor) {
+	this.valor = valor;
+
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+	return valor;
+    }
 
 }
